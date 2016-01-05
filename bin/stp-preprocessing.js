37772a27e4f2c7c9f7d4b23e90be6db580457a57
rodklerc <Robin.Deklerck@UGent.be>
2016-01-05 13:32:15 +0100

    use client module

--- bin/stp-preprocessing.js
+++ bin/stp-preprocessing.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
-var Fetcher = require('../lib/Fetcher.js'),
+var Fetcher = require('lc-client').Fetcher,
     GraphBuilder = require('../lib/buildGraph.js'),
     path = require('path'),
     fs = require('fs'),
-    program = require('commander');
+    program = require('commander'),
+    assert = require('assert');
+
+var MongoClient = require('mongodb').MongoClient;
 
 // TODO:
 // 1. parameters: begin- en einddatum, configuratie, db of geheugen
@@ -12,10 +15,17 @@ var Fetcher = require('../lib/Fetcher.js'),
 //    ook opslag in het geheugen voor kleinere netwerken
 // 3. code beter opsplitsen
 
-var fetcher = new Fetcher({"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]});
-var graphBuilder = new GraphBuilder({"connectionString" : "mongodb://localhost:27017/qsdf"});
+var mongoDb;
+MongoClient.connect("mongodb://localhost/stp",
+                    function(err, db) {
+                        assert.equal(null, err);
+                        mongoDb = db;
+                    });
 
-/*fetcher.buildConnectionsStream({"departureTime": new Date("2015-10-10T10:00")},
+var fetcher = new Fetcher({"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]});
+var graphBuilder = new GraphBuilder(mongoDb);
+
+fetcher.buildConnectionsStream({"departureTime": new Date("2015-10-10T10:00")},
                                function (connectionStream) {
-				   connectionStream.on('data', graphBuilder.handleConnection);
-                               });*/
+                                  connectionStream.pipe(graphBuilder);
+                               });
