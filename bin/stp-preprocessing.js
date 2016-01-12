5c65b1982926c33a94eaaba0996ec1487597adc3
rodklerc <Robin.Deklerck@UGent.be>
2016-01-12 15:47:19 +0100

    refactoring with bug

--- bin/stp-preprocessing.js
+++ bin/stp-preprocessing.js
@@ -1,27 +1,22 @@
 #!/usr/bin/node
 
-var Fetcher = require('lc-client').Fetcher,
-    GraphBuilder = require('../lib/buildGraph.js'),
+var CalculateLocalPaths = require('../lib/localPaths.js'),
     ClusterAlgorithm = require('../lib/cluster.js'),
-    CalculateLocalPaths = require('../lib/localPaths.js'),
-    path = require('path'),
-    fs = require('fs'),
-    program = require('commander'),
-    assert = require('assert'),
-    JSONStream = require('JSONStream');
-var LocalPathBuilder = require('../lib/localPathBuilder.js');
-var DAGBuilder = require('../lib/dagBuilder.js');
+    Fetcher = require('lc-client').Fetcher,
+    GraphBuilder = require('../lib/buildGraph.js');
 
-// TODO:
-// 1. Input parameters: start and end date to limit the number of connections
-//    to process + configuration parameters
+// TODO: input parameters etc.
 
 var fetcher = new Fetcher({"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]});
-var graphBuilder = new GraphBuilder(null);
+var graphBuilder = new GraphBuilder();
 fetcher.buildConnectionsStream({"departureTime": new Date("2015-10-10T10:00")},
                                function (connectionStream) {
                                   connectionStream
                                     .pipe(graphBuilder)
                                     .pipe(new ClusterAlgorithm(100))
-                                    .pipe(new CalculateLocalPaths(new Date("2015-10-10T10:00"), {"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]}));
+                                    .pipe(new CalculateLocalPaths(new Date("2015-10-10T10:00"),
+                                                                  {"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]})
+                                    .on('data', function (result) {
+                                      console.log(result);
+                                    }));
                                });
