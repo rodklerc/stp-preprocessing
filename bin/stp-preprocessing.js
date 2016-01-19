40be5fb349934036a5b0141e3c6888564cb103aa
rodklerc <Robin.Deklerck@UGent.be>
2016-01-19 13:57:01 +0100

    remove debug code

--- bin/stp-preprocessing.js
+++ bin/stp-preprocessing.js
@@ -3,6 +3,7 @@
 var CalculateLocalPaths = require('../lib/localPaths.js'),
     ClusterAlgorithm = require('../lib/cluster.js'),
     Fetcher = require('lc-client').Fetcher,
+    fs = require('fs'),
     GraphBuilder = require('../lib/buildGraph.js');
 
 // TODO: input parameters etc.
@@ -11,9 +12,15 @@ var fetcher = new Fetcher({"entrypoints" : ["http://belgianrail.linkedconnection
 var graphBuilder = new GraphBuilder();
 fetcher.buildConnectionsStream({"departureTime": new Date("2015-10-10T10:00")},
                                function (connectionStream) {
-                                  connectionStream
+                                 var lpb = new CalculateLocalPaths(new Date("2015-10-10T06:00"),
+                                                               {"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]});
+                                 lpb.on('data', function (result) {
+                                   fs.writeFileSync('stp-output.json',
+                                                    JSON.stringify(result),
+                                                    'utf-8');
+                                 });
+                                 connectionStream
                                     .pipe(graphBuilder)
                                     .pipe(new ClusterAlgorithm(100))
-                                    .pipe(new CalculateLocalPaths(new Date("2015-10-10T10:00"),
-                                                                  {"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]}));
+                                    .pipe(lpb);
                                });
