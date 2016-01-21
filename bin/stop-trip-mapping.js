6454b2a98d169179764a47398ad86d30a6e67a0d
rodklerc <Robin.Deklerck@UGent.be>
2016-01-21 14:09:27 +0100

    forgot to add stop-trip mapping

---
+++ bin/stop-trip-mapping.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+var Fetcher = require('lc-client').Fetcher,
+    fs = require('fs'),
+    program = require('commander'),
+    StopTripMapper = require('../lib/stopTripMapper.js');
+
+function list(val) {
+  return val.split(',');
+}
+
+program
+  .version('0.0.1')
+  .usage('[options] <output filename>')
+  .option('-e, --entrypoints <es>', 'List of linked connections entry point URLs', list)
+  .option('-s, --startTime <t>', 'Scan connections departing no earlier than the point in time represented by this date and time string')
+  .arguments('<output>')
+  .action(function (output, opts) {
+    outputFilename = output;
+  })
+  .parse(process.argv);
+
+var entryPoints = {"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]};
+var startTime = new Date("2015-10-10T03:00");
+
+if (typeof outputFilename === 'undefined') {
+  console.error('Please specify an output file.');
+  process.exit(1);
+}
+
+var fetcher = new Fetcher(entryPoints);
+fetcher.buildConnectionsStream({"departureTime": startTime},
+                               function (connectionStream) {
+                                 var stm = new StopTripMapper();
+                                 connectionStream.pipe(stm);
+                                 stm.on('data', function (result) {
+                                   fs.writeFileSync(outputFilename,
+                                                    JSON.stringify(result),
+                                                    'utf-8');
+                                 });
+                               });
