066e73bbf6458751303a75397bf4a4956e5dafd1
rodklerc <Robin.Deklerck@UGent.be>
2016-01-27 15:47:53 +0100

    now uses routes instead of trips

---
+++ bin/stop-route-mapping.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+
+var Fetcher = require('lc-client').Fetcher,
+    fs = require('fs'),
+    program = require('commander'),
+    StopRouteMapper = require('../lib/stopRouteMapper.js');
+
+function list(val) {
+  return val.split(',');
+}
+
+program
+  .version('0.0.1')
+  .usage('[options] <output filename>')
+  .option('-e, --entrypoints <es>', 'List of linked connections entry point URLs', list)
+  .option('-s, --startTime <t>', 'Scan connections departing no earlier than the point in time represented by this date and time string')
+  .arguments('<output>')
+  .action(function (output, opts) {
+    outputFilename = output;
+  })
+  .parse(process.argv);
+
+var entryPoints = {"entrypoints" : ["http://belgianrail.linkedconnections.org/connections"]};
+var startTime = new Date("2016-01-08T07:29");
+
+if (program.entrypoints) entryPoints = {"entrypoints" : program.entrypoints};
+if (program.startTime) startTime = new Date(program.startTime);
+
+console.log(entryPoints);
+console.log(startTime);
+
+if (typeof outputFilename === 'undefined') {
+  console.error('Please specify an output file.');
+  process.exit(1);
+}
+
+var fetcher = new Fetcher(entryPoints);
+fetcher.buildConnectionsStream({"departureTime": startTime},
+                               function (connectionStream) {
+                                 var srm = new StopRouteMapper();
+                                 connectionStream.pipe(srm);
+                                 srm.on('data', function (result) {
+                                   fs.writeFileSync(outputFilename,
+                                                    JSON.stringify(result),
+                                                    'utf-8');
+                                 });
+                               });
